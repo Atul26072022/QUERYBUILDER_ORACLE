a50360d56210da2078261fd099fe5c58409919d5
rahul <rahul.antino@gmail.com>
2023-03-06 00:44:28 +0530

    insert_command_in_table

---
+++ oracle_project/oracle_app/oracle.py
@@ -0,0 +1,32 @@
+import cx_Oracle
+
+dict1 = {
+    "id" : 9,
+    "name" : "rahul11",
+    "age" : 23
+}
+
+
+try:
+    with cx_Oracle.connect('system/1234') as co:
+        print("Connected")
+        cur = co.cursor()
+
+
+        cur.execute("INSERT INTO ORACLE_APP_STUDENT VALUES (:1,:2,:3)",[dict1["id"],dict1["name"],dict1["age"]])
+        # cur.execute("INSERT INTO ORACLE_APP_STUDENT VALUES (?,?,?)",(dict1["id"],dict1["name"],dict1["age"]))
+        # cur.execute(f"INSERT INTO ORACLE_APP_STUDENT VALUES ({dict1['id']},{dict1['name']},{dict1['age']})")
+        # cur.execute('SELECT * FROM ORACLE_APP_STUDENT')
+        # x = cur.fetchall()
+        # print(x)
+        co.commit()
+        cur.close()
+        print("DATA INSERTED")
+        
+        
+        
+        
+except Exception as e:
+    print("Error: ",str(e))
+
+        
