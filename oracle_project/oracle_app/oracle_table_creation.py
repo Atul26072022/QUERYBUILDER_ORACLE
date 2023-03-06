83b4471d03360ff013924efd20d6706c3d752447
rahul <rahul.antino@gmail.com>
2023-03-06 14:42:51 +0530

    dynamic_table_creation

---
+++ oracle_project/oracle_app/oracle_table_creation.py
@@ -0,0 +1,30 @@
+import cx_Oracle
+from rest_framework.response import Response
+
+dict2 = {
+    "name" : "esd",
+    "Company" :  "Antino",
+    "emp" : "employee_id" }
+
+
+
+def create_table(self,dict2):
+    
+
+
+    try:
+        with cx_Oracle.connect('system/1234')as co:
+            print("Connected")
+            cur=co.cursor()
+            cur.execute(f"CREATE TABLE {dict2['name']}( {dict2['Company']} varchar2(10), {dict2['emp']} varchar2(10))")
+            print("Table Created")
+            cur.close()
+        return ({"msg : Table Created successfully"})
+                    
+    except Exception as e:
+        # print("Error: ",str(e))
+        return ({"msg" : "Table not created", "error" : str(e)})
+
+
+     
+    
